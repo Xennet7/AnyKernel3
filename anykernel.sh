ecedf5876b7a1734522d0e232856007b4716e3c8
XENNET <xennet@rediffmail.com>
2026-07-11 12:38:12 +0530

    Change kernel branding from Wild Kernels to XENNET
    Updated kernel branding and removed old references.

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Kernels by TheWildJames aka Morgan Weedman
+kernel.string=XENNET Kernels by Xennet The Terminator
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -59,25 +59,5 @@ else
 fi
 
 ui_print " "
-ui_print "WildKernels Telegram Channel:"
-ui_print "https://t.me/WildKernelsTG"
-ui_print " "
-ui_print "WildKernels Website:"
-ui_print "https://wildkernels.dev"
-ui_print " "
-ui_print "GKI_KernelSU_SUSFS GitHub Repository:"
-ui_print "https://github.com/WildKernels/GKI_KernelSU_SUSFS"
-ui_print "GKI kernels with KernelSU and SUSFS."
-ui_print " "
-ui_print "OnePlus_KernelSU_SUSFS GitHub Repository:"
-ui_print "https://github.com/WildKernels/OnePlus_KernelSU_SUSFS"
-ui_print "OnePlus kernels with KernelSU and SUSFS."
-ui_print " "
-ui_print "Samsung_KernelSU_SUSFS GitHub Repository:"
-ui_print "https://github.com/WildKernels/Samsung_KernelSU_SUSFS"
-ui_print "Samsung kernels with KernelSU and SUSFS."
-ui_print " "
-ui_print "If you have any questions or need support, feel free to join our Telegram channel!" 
-ui_print " "
-ui_print "Thank you for using Wild Kernels! - TheWildJames"
+ui_print "Thank you for using XENNET Kernels! - Xennet The Terminator"
 ui_print " "
